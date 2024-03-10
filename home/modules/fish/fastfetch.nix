f8af4406ad96fe1f65b828dc21174d3f70e34621
Jacob Highfield <camperboy1000@gmail.com>
2024-03-10 14:29:59 -0400

    Getting package count is slow on NixOS

--- home/modules/fish/fastfetch.nix
+++ home/modules/fish/fastfetch.nix
@@ -10,6 +10,6 @@
   home.file.".config/fastfetch/config.conf".text = ''
     --percent-type 3
 
-    --structure Title:Separator:OS:Host:Kernel:Uptime:Packages:Shell:DE:WM:WMTheme:Theme:Icons:Terminal:CPU:GPU:Memory:Swap:Disk:Battery:PowerAdapter:Break:DateTime:Media:Break:Colors
+    --structure Title:Separator:OS:Host:Kernel:Uptime:Shell:DE:WM:WMTheme:Theme:Icons:Terminal:CPU:GPU:Memory:Swap:Disk:Battery:PowerAdapter:Break:DateTime:Media:Break:Colors
   '';
 }
