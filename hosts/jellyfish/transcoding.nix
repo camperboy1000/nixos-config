38ee31d2a6aa444bdaaf99026bf981ec1051dd61
Jacob Highfield <camperboy1000@gmail.com>
2024-03-17 18:38:54 -0400

    Copy transcoding config from wiki

--- hosts/jellyfish/transcoding.nix
+++ hosts/jellyfish/transcoding.nix
@@ -1,7 +1,17 @@
 { config, pkgs, ... }:
 
 {
-  hardware.opengl.enable = true;
+  hardware.opengl = {
+    enable = true;
+
+    extraPackages = with pkgs; [
+      intel-media-driver
+      (vaapiIntel.override { enableHybridCodec = true; })
+      vaapiVdpau
+      libvdpau-va-gl
+      intel-compute-runtime
+    ];
+  };
 
   users.groups = {
     video = {
